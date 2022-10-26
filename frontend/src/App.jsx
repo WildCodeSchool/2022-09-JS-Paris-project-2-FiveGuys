39d6f8cee4e49d85b1717acde9d8fe3b36fb70cc
Quentin Feuvrais <feuvrais.quentin@gmail.com>
2022-10-26 14:48:39 +0200

    add skeleton app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import "./style/App.css";
+import React, { useState } from "react";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div>Bonjour</div>;
+  const [currentPage, setCurrentPage] = useState("");
+  // useEffect(() => {
+  //   console.log(currentPage);
+  // }, [currentPage]);
+
+  return (
+    <div>
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {currentPage === "news" && <div>News</div>}
+      {currentPage === "contact" && <div>Contact</div>}
+      {currentPage === "about" && <div>About Us</div>}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
