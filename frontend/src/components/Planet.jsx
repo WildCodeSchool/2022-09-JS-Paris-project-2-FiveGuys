6a0de878059816fced8b00fb68c28216cb524224
Rebonace <celiarebonato@gmail.com>
2022-11-03 10:27:46 +0100

    commit de pull

--- frontend/src/components/Planet.jsx
+++ frontend/src/components/Planet.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "../style/Planet.css";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import icon from "../style/assets/raincloud.png";
 
 function Planet({ size, name, pic, bgsize, position }) {
   const planetStyle = {
@@ -16,10 +19,23 @@ function Planet({ size, name, pic, bgsize, position }) {
 
   const elem = (
     <div className="planet-container" style={containerPosition}>
-      <button type="button" className="planet-button" style={planetStyle}>
-        {" "}
-      </button>
-      <div className="planet-name">{name}</div>
+      <Tippy
+        arrow={false}
+        content={
+          <div className="planet-tooltip">
+            <h1 className="tooltip-title">{name}</h1>
+
+            <img className="raincloud" src={icon} alt="icon" />
+
+            <h2 className="tooltip-degree"> 35° </h2>
+          </div>
+        }
+        placement="bottom"
+      >
+        <button type="button" className="planet-button" style={planetStyle}>
+          {" "}
+        </button>
+      </Tippy>
     </div>
   );
   return elem;
